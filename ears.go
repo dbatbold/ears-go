300538f55e5c898eb384c6e15ea12689c41c2ba9
Batbold <dbatbold@gmail.com>
2025-01-11 12:49:30 -0800

    use etag

--- ears.go
+++ ears.go
@@ -9,25 +9,39 @@ import (
 	"time"
 )
 
+var (
+	notified = map[string]bool{}
+)
+
 func main() {
+	date := time.Now()
 	for {
 		run()
 		time.Sleep(time.Hour)
+		if date.Day() != time.Now().Day() {
+			notified = map[string]bool{}
+			date = time.Now()
+		}
 	}
 }
 
-func run() {
+func parseConfig() (list []Monitor) {
 	earsJson, err := ioutil.ReadFile("ears.json")
 	if err != nil {
 		panic(err)
 	}
-	var list []Monitor
 	if err := json.Unmarshal(earsJson, &list); err != nil {
 		panic(err)
 	}
+	return
+}
+
+func run() {
+	list := parseConfig()
 
 	client := &http.Client{}
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		// prevent redirects
 		return http.ErrUseLastResponse
 	}
 
@@ -36,11 +50,17 @@ func run() {
 		if err != nil {
 			panic(err)
 		}
-		// req.Header.Set("accept", "*/*")
+		if len(monitor.Etag) > 0 {
+			req.Header.Set("If-None-Match", monitor.Etag)
+		}
 		// req.Header.Set("User-Agent", "curl/8.7.1")
 		res, err := client.Do(req)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "HEAD request failed '%s': HTTP %s\n", monitor.Url, res.StatusCode)
+			if res != nil {
+				fmt.Fprintf(os.Stderr, "HEAD request failed '%s': HTTP %s\n", monitor.Url, res.StatusCode)
+			} else {
+				fmt.Fprintf(os.Stderr, "HEAD request failed '%s'\n", monitor.Url)
+			}
 			continue
 		}
 		// fmt.Println(res.StatusCode)
@@ -50,17 +70,22 @@ func run() {
 		if len(monitor.Location) > 0 {
 			location := res.Header.Get("Location")
 			if location != monitor.Location {
-				fmt.Println(monitor.Name + ":")
-				fmt.Println("\t" + monitor.Location)
-				fmt.Println("\t" + location)
+				monitor.print(location)
 			}
 		}
-		if len(monitor.Etag) > 0 {
-			etag := res.Header.Get("etag")
-			if etag != monitor.Etag {
-				fmt.Println(monitor.Name + ":")
-				fmt.Println("\t" + monitor.Etag)
-				fmt.Println("\t" + etag)
+		if len(monitor.Etag) > 0 && res.StatusCode != http.StatusNotModified {
+			monitor.print(res.Header.Get("etag"))
+		}
+		if len(monitor.LastModified) > 0 {
+			modified := res.Header.Get("last-modified")
+			if modified != monitor.LastModified {
+				monitor.print(modified)
+			}
+		}
+		if len(monitor.Redirect) > 0 {
+			location := res.Header.Get("location")
+			if location != monitor.Redirect {
+				monitor.print(location)
 			}
 		}
 	}
@@ -68,8 +93,33 @@ func run() {
 }
 
 type Monitor struct {
-	Name     string `json:"name"`
-	Url      string `json:"url"`
-	Location string `json:"location"`
-	Etag     string `json:"etag"`
+	Name         string `json:"name"`
+	Url          string `json:"url"`
+	Location     string `json:"location"`
+	Etag         string `json:"etag"`
+	LastModified string `json:"last_modified"`
+	Redirect     string `json:"redirect"`
+}
+
+func (m *Monitor) print(diff string) {
+	if notified[m.Url] {
+		return
+	}
+	now := time.Now()
+	fmt.Println(now.Format(time.RFC3339), m.Name, m.Url)
+	if len(m.Redirect) > 0 {
+		fmt.Println("\t" + m.Redirect)
+		fmt.Println("\t" + diff)
+		notified[m.Url] = true
+	}
+	if len(m.LastModified) > 0 {
+		fmt.Println("\t" + m.LastModified)
+		fmt.Println("\t" + diff)
+		notified[m.Url] = true
+	}
+	if len(m.Location) > 0 {
+		fmt.Println("\t" + m.Location)
+		fmt.Println("\t" + diff)
+		notified[m.Url] = true
+	}
 }
